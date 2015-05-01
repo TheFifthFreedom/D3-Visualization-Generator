316dbbf6c2849609b052760759a170dd27205244
Laurent Mazouer <laurent.mazouer@gmail.com>
2015-05-01 14:20:53 -0400

    Implemented PID Wall

---
+++ js/index.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+
+  // Disabling submit button until all fiels have been populated
+  $('#regularLogin input').keyup(function() {
+        var empty = false;
+        $('#regularLogin input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#laddaButton').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+            $('#laddaButton').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+    });
+    $('#specialLogin input').keyup(function() {
+          var empty = false;
+          $('#specialLogin input').each(function() {
+              if ($(this).val() == '') {
+                  empty = true;
+              }
+          });
+
+          if (empty) {
+              $('#laddaButton2').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+          } else {
+              $('#laddaButton2').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+          }
+      });
+
+    // Form submission
+    $("#regularLogin").submit(function(){
+        //Button spniwheel
+        var l = Ladda.create(document.getElementById('laddaButton'));
+        l.start();
+
+        //AJAX call
+        var postdata = {pid: $("#pid").val(), pmName: $("#pmName").val(), userName: $("#userName").val()};
+        $.post("/authenticate", postdata, function(data){
+            jsonData = JSON.parse(data);
+            // First clear all previous validation states
+            if ($('.pid').hasClass('has-success')){$('.pid').removeClass('has-success');}
+            else if ($('.pid').hasClass('has-error')){$('.pid').removeClass('has-error');}
+            $('#pidHelp').text('');
+
+            if ($('.pmName').hasClass('has-success')){$('.pmName').removeClass('has-success');}
+            else if ($('.pmName').hasClass('has-error')){$('.pmName').removeClass('has-error');}
+            $('#pmHelp').text('');
+
+            if ($('.userName').hasClass('has-success')){$('.userName').removeClass('has-success');}
+            else if ($('.userName').hasClass('has-error')){$('.userName').removeClass('has-error');}
+            $('#userHelp').text('');
+
+            // Then check the data and establish new ones if needed
+            // Test PID
+            if (jsonData['pid']){
+                $('.pid').addClass('has-success');
+            }
+            else{
+                $('.pid').addClass('has-error');
+                $('#pidHelp').text('Invalid PID');
+            }
+            //Test PM
+            if (jsonData['pmName']){
+                $('.pmName').addClass('has-success');
+            }
+            else{
+                $('.pmName').addClass('has-error');
+                $('#pmHelp').text('That\'s not a PM');
+            }
+            // Test User
+            if (jsonData['userName']){
+                $('.userName').addClass('has-success');
+            }
+            else{
+                $('.userName').addClass('has-error');
+                $('#userHelp').text('That\'s not a Sapient employee');
+            }
+
+            // Reload page if everything checks out
+            if (jsonData['pid'] && jsonData['pmName'] && jsonData['userName']){
+                location.reload(true);
+            }
+        }).always(function() { l.stop(); });
+        return false;
+    });
+    $("#specialLogin").submit(function(){
+        //Button spniwheel
+        var l = Ladda.create(document.getElementById('laddaButton'));
+        l.start();
+        
+        var postdata = {password: $("#master").val()};
+        $.post("/master_authenticate", postdata, function(data){
+            jsonData = JSON.parse(data);
+            // First clear all previous validation states
+            if ($('.master').hasClass('has-success')){$('.master').removeClass('has-success');}
+            else if ($('.master').hasClass('has-error')){$('.master').removeClass('has-error');}
+            $('#masterHelp').text('');
+
+            // Then check the data and establish new ones if needed
+            if (jsonData['password']){
+                $('.master').addClass('has-success');
+                location.reload(true);
+            }
+            else{
+                $('.master').addClass('has-error');
+                $('#masterHelp').text('Wrong password');
+            }
+        }).always(function() { l.stop(); });
+        return false;
+    });
+});
--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 import cherrypy
+import threading
+import time
+import json
 import csv
 import os
+import requests
+from requests_ntlm import HttpNtlmAuth
+from ldap3 import Server, Connection, SIMPLE, SYNC, ASYNC, SUBTREE, ALL
+
+valid_sessions=[]
+
+def purge_session_id(session_id):
+    if session_id in valid_sessions:
+        valid_sessions.remove(session_id)
 
 class GraphingServer(object):
+
     @cherrypy.expose
     def index(self):
-        response = """
+
+        no_paywall_response = """
             <!DOCTYPE html>
             <meta charset="utf-8">
             <html>
@@ -18,7 +32,6 @@ class GraphingServer(object):
                     </style>
                     <title>D3 Visualization Generator</title>
                 </head>
-
                 <body>
                     <form action="upload" method="post" enctype="multipart/form-data">
                         Select CSV to upload:
@@ -34,10 +47,108 @@ class GraphingServer(object):
                 </body>
             </html>
         """
-        return response
+
+        # Checking cookies
+        request_cookie = cherrypy.request.cookie
+        if request_cookie.keys() and request_cookie['session_id'].value in valid_sessions:
+            return no_paywall_response
+        else:
+            return open(os.path.abspath('html/index.html'))
+
+    @cherrypy.expose
+    def authenticate(self, pid, pmName, userName):
+        #PID Validation (to make sure it's a valid PID)
+        pidOK = True
+        session = requests.Session()
+        session.auth = HttpNtlmAuth('sapient.com\\lmazou', 'Kus@n@g4', session)
+        r1 = session.get('http://timetracking.sapient.com/')
+
+        headers = {
+            'Host': 'timetracking',
+            'Connection': 'keep-alive',
+            'Content-Length': '15',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Origin': 'http://timetracking',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36',
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Referer': 'http://timetracking/EnterHours.aspx',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,en;q=0.8,fr;q=0.6'
+        }
+        payload = json.dumps({'Pid' : pid})
+
+        r2 = session.post('http://timetracking/EnterHours.aspx/GetPidInfo', headers=headers, data=payload)
+        if 'Not a valid PID!' in r2.text or 'not a valid value' in r2.text or pid == "63492" or pid == "104478":
+            pidOK = False
+
+        #PM Validation (to make sure it's actually a Sapient employee and a PM)
+        pmOK = False
+        pmEmail = ''
+        s = Server('coloads1.sapient.com', port = 3268, get_info = ALL)
+        c = Connection(s, auto_bind = True, client_strategy = SYNC, user='SAPIENT\lmazou', password='Kus@n@g4', authentication=SIMPLE, check_names=True)
+        c.search('OU=Employee Users,OU=Accounts,DC=sapient,DC=com','(cn=' + pmName + ')', SUBTREE, attributes = ['description', 'mail'])
+        response = c.response
+
+        if response and 'Program Management' in response[0]['attributes']['description'][0]:
+            pmOK = True
+            pmEmail = response[0]['attributes']['mail']
+
+        #User validation (to make sure it's an actual Sapient employee)
+        userOK = False
+        c.search('OU=Employee Users,OU=Accounts,DC=sapient,DC=com','(cn=' + userName + ')', SUBTREE, attributes = ['description', 'mail'])
+        response = c.response
+
+        if response:
+            userOK = True
+
+        if pidOK and pmOK and userOK: # Everything checks out
+            # Create authentication spoiler
+            response_cookie = cherrypy.response.cookie
+            response_cookie['session_id'] = cherrypy.lib.sessions.Session.generate_id(self)
+            response_cookie['session_id']['path'] = '/'
+            response_cookie['session_id']['expires'] = 1800
+            # Whitelist the authentication cookie
+            valid_sessions.append(response_cookie['session_id'].value)
+            # Start a thread to purge that authentication cookie from the whitelist in 30min
+            t = threading.Timer(1800, purge_session_id, [response_cookie['session_id'].value])
+            t.start()
+            # Email PM
+            email_string = "This is an automatically generated e-mail to let you know that " + userName + " accessed the Sapient SEO Toolset on " + time.strftime("%c") + ", which will bill 30 minutes to PID#" + pid + ". Feel free to reply to this e-mail should you have any concerns."
+            requests.post(
+                "https://api.mailgun.net/v3/sandbox11b4340382ad4275897e64b56b9a099e.mailgun.org/messages",
+                auth=("api", "key-427a6dea1d992181f35def2e501d8f27"),
+                data={"from": "Laurent Mazouer <lmazouer@sapient.com>",
+                      "to": pmEmail,
+                      "subject": "*** Sapient SEO Tools Usage Notification ***",
+                      "text": email_string})
+            # Log the authentication
+            f = open(os.path.abspath('sessions_log/log'), 'a')
+            f.write(time.strftime("%c") + " | PID: " + pid + " | PM: " + pmName + " | User: " + userName + "\n")
+            f.close()
+
+        return json.dumps(dict(pid = pidOK, pmName = pmOK, userName = userOK))
+
+    @cherrypy.expose
+    def master_authenticate(self, password):
+        passwordOK = False
+        if password == 'MargaretThatcheris110%SEXY':
+            passwordOK = True
+            response_cookie = cherrypy.response.cookie
+            response_cookie['session_id'] = cherrypy.lib.sessions.Session.generate_id(self)
+            response_cookie['session_id']['path'] = '/'
+            response_cookie['session_id']['expires'] = 1800
+            valid_sessions.append(response_cookie['session_id'].value)
+            t = threading.Timer(1800, purge_session_id, [response_cookie['session_id'].value])
+            t.start()
+        return json.dumps(dict(password = passwordOK))
 
     @cherrypy.expose
     def upload(self, input_file, viz_type):
+        # Checking to see if the cookie is still there
+        request_cookie = cherrypy.request.cookie
+        if (not request_cookie.keys()) or (request_cookie['session_id'].value not in valid_sessions):
+            raise cherrypy.HTTPRedirect('/index')
 
         all_data = bytearray()
         while True:
